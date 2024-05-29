cd3493c1e771b063bf39bbc95d69ff40166e6595
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-05-29 12:49:43 +0200

    Migrate to datafree

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,7 @@
 from typing import Any, Tuple
 
+from flops_utils.ml_repo_building_blocks import load_dataset
 from flops_utils.ml_repo_templates import DataManagerTemplate
-from flwr_datasets import FederatedDataset
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, Normalize, ToTensor
 
@@ -12,10 +12,11 @@ class DataManager(DataManagerTemplate):
 
     def _prepare_data(self, partition_id=1) -> None:
         """Load partition CIFAR10 data."""
-        fds = FederatedDataset(dataset="cifar10", partitioners={"train": 3})
-        partition = fds.load_partition(partition_id)
+        dataset = load_dataset()
+        dataset.set_format("numpy")
+
         # Divide data on each node: 80% train, 20% test
-        partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
+        dataset_train_test = dataset.train_test_split(test_size=0.2, seed=42)
         pytorch_transforms = Compose(
             [ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
         )
@@ -25,11 +26,11 @@ class DataManager(DataManagerTemplate):
             batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
             return batch
 
-        partition_train_test = partition_train_test.with_transform(apply_transforms)
+        dataset_train_test = dataset_train_test.with_transform(apply_transforms)
         self.trainloader = DataLoader(
-            partition_train_test["train"], batch_size=32, shuffle=True
+            dataset_train_test["train"], batch_size=32, shuffle=True
         )
-        self.testloader = DataLoader(partition_train_test["test"], batch_size=32)
+        self.testloader = DataLoader(dataset_train_test["test"], batch_size=32)
 
     def get_data(self) -> Tuple[Any, Any]:
         return self.trainloader, self.testloader
